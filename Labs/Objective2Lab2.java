0cab9b3d2d9a0f55d4496c31164c0c3a4aae07f3
sckalina <sckalina@yahoo.com>
2019-05-01 12:38:44 -0600

    finish Labs

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Stephanie Williams";
+    int age = 39;
+    String state = "Illinois";
+    System.out.println( " Name : " + name + " Age : " + age + " State : " + state );
+  }
+}
