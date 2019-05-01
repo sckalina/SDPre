23c3f9f0b9d9c92c692f89e78f6098adfea37331
sckalina <sckalina@yahoo.com>
2019-05-01 07:47:12 -0600

    finish JavaTheHardWay

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main( String[] args ) {
-    System.out.println( "I am determined to learn code." );
+    System.out.println( "I, Stephanie Williams, am determined to learn code." );
     System.out.println( "Today's date is Wednesday May 1, 2019.");
   }
 }
