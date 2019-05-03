b10b03a162964e9cfe5cac69d7a69332fc2c861b
sckalina <sckalina@yahoo.com>
2019-05-03 12:09:10 -0600

    finish Labs

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,12 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int count = 1, sum = 0;
+
+      while (count <=20) {
+        sum += count;
+        count++;
+      }
+
+      System.out.println(sum);
+  }
+}
